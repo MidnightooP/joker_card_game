4c8d9a677efd01cd64ad606702d7f123f7d0d141
MidnightooP <antoniaivanova306@gmail.com>
2023-02-14 19:08:28 +0100

    hey

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
+import { Progress } from '@chakra-ui/react';
+import { Heading } from '@chakra-ui/react';
+
+import React from 'react';
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+  Stack,
+  Image,
+  Button,
+} from '@chakra-ui/react';
+
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+} from '@chakra-ui/react';
+
+function importAll(r) {
+  let images = {};
+  r.keys().map((item, index) => {
+    images[item.replace('./', '')] = r(item);
+  });
+  return images;
+}
+
+const images = importAll(
+  require.context('./cardfixed', false, /\.(png|jpe?g|svg)$/)
+);
+
+console.log(images);
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Image src={images['2_of_clubs.svg']} />
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default App;
